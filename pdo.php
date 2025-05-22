97d20c48843138ccf5ca68c8df37c65126155886
HuongDS <146117043+HuongDS@users.noreply.github.com>
2025-05-23 00:47:25 +0700

    Update pdo.php

--- pdo.php
+++ pdo.php
@@ -1,17 +1,15 @@
 <?php
+$host = 'centerbeam.proxy.rlwy.net';
+$port = '31652';
+$db   = 'js';
+$user = 'alan';
+$pass = 'alan';
 
-    $hostName = "localhost";
-    $userName = "alan";
-    $password = "alan";
-    $dbName = "js";
-
-    try {
-        $pdo = new PDO("mysql:host=$hostName;dbname=$dbName",$userName, $password);
-        
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        
-    } catch(PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
-
+$pdo = null;
+try {
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "<p style='color:red'>Connection failed: " . htmlentities($e->getMessage()) . "</p>";
+}
 ?>
