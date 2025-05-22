26e6777869395ed4e892644d841e0033e1073322
HuongDS <146117043+HuongDS@users.noreply.github.com>
2025-05-23 03:07:12 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,67 +1,82 @@
 <?php 
-require_once "pdo.php";
-session_start();
+    require_once "pdo.php";
+    session_start();
 
-if (isset($_POST['Cancel'])) {
-    header("Location: index.php");
-    return;
-}
+    if (isset($_POST['cancel'])) {
+        header("Location: index.php");
+        return;
+    }
 
-if (isset($_POST['Delete']) && isset($_POST['profile_id'])) {
-    $sql = "DELETE FROM Profile WHERE profile_id = :pi";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute([':pi' => $_POST['profile_id']]);
+    if (isset($_POST['profile_id']) ) {
 
-    $_SESSION['success'] = "Profile deleted";
-    header("Location: index.php");
-    return;
-}
+        $sql = "DELETE FROM Profile WHERE profile_id = :pi";
 
-if (!isset($POST['profile_id'])) {
-    $_SESSION['error'] = "Could not load profile";
-    header("Location: index.php");
-    return;
-}
+        $statement = $pdo->prepare($sql);
 
-$stmt = $pdo->prepare("SELECT * FROM Profile WHERE profile_id = :profile_id");
-$stmt->execute([':profile_id' => $_GET["profile_id"]]);
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $statement->execute(array(
+            ":pi" => $_POST['profile_id']
+        ));
 
-if ($row === false) {
-    $_SESSION['error'] = "Could not load profile";
-    header("Location: index.php");
-    return;
-}
+        $_SESSION['success'] = "Profile deleted";
+        header("Location: index.php");
+        return;
 
-$first_name = htmlentities($row["first_name"]);
-$last_name = htmlentities($row["last_name"]);
-$email = htmlentities($row["email"]);
-$headline = htmlentities($row["headline"]);
-$summary = htmlentities($row["summary"]);
+    }
+    
+    if (!isset($_GET['profile_id'])) {
+        $_SESSION['error'] = "Could not load Profile";
+        header("Location: index.php");
+        return;
+    }
+    
+    $statement = $pdo->prepare("SELECT * FROM Profile WHERE profile_id = :profile_id");
+    
+    $statement->execute(array(
+        ":profile_id" => $_GET["profile_id"],
+    ));
+
+    $row = $statement->fetch(PDO::FETCH_ASSOC);
+
+    if ($row == false) {
+        $_SESSION['error'] = "Could not load Profile";
+        header("Location: index.php");
+        return;
+    }
+
+    $first_name = htmlentities($row["first_name"]);
+    $last_name = htmlentities($row["last_name"]);
+    $email = htmlentities($row["email"]);
+    $headline = htmlentities($row["headline"]);
+    $summary = htmlentities($row["summary"]);
 ?>
-
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Huong Dang 111574d6</title>
-</head>
+
 <body>
-    <h1>Deleting Profile</h1>
+
+    <h1>Deleteing Profile </h1>
 
     <?php 
-    if (isset($_SESSION['error'])) {
-        echo "<p style='color: red'>".$_SESSION['error']."</p>";
-        unset($_SESSION['error']);
-    }
+        if (isset($_SESSION['error'])) {
+            echo "<p style='color: red'>".$_SESSION['error']."</p>";
+            unset($_SESSION['error']);
+        }
     ?>
 
-    <p>First Name: <?= $first_name ?></p>
-    <p>Last Name: <?= $last_name ?></p>
+    <p>
+        First Name: <?php echo $first_name; ?>
+    </p>
+    <p>
+        Last Name: <?php echo $last_name; ?>
+    </p>
 
     <form method="POST">
-        <input type="hidden" name="profile_id" value="<?= htmlentities($_GET['profile_id']) ?>" />
+        <p>
+            <input type="hidden" name="profile_id" value="<?= $_GET['profile_id'] ?>" />
+        </p>
         <input type="submit" name="delete" value="Delete" />
         <input type="submit" name="cancel" value="Cancel" />
     </form>
 </body>
+
 </html>
